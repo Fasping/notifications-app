1c19c4dfa02259e0d56e62640c81ac6025b0cdc3
Fernando Cases <nandocasesgarcia@gmail.com>
2023-01-25 13:41:47 +0100

    Add: logic to mark as read all unread notifications

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 // Get DOM Elements
 
 const markAllAsReadElement = document.querySelector('.header_link')
+
+//Listend for click events 
+
+markAllAsReadElement.addEventListener('click', markAllAsRead)
+
+//Mark all notifications as read
+function markAllAsRead() {
+    //Get all unread notifications
+    const unreadNotifications = document.querySelectorAll(".notification--unread");
+    unreadNotifications.forEach((notification) =>
+      notification.classList.remove('notification--unread')
+    );
+}
+
